4668725ec2d8220bf8109a3be7b842a59d325d32
Shaifullah Arfan <shaifullaharfan17@gmail.com>
2020-07-18 20:50:33 +0600

    adding nav-bar toggle

--- main.js
+++ main.js
@@ -54,28 +54,6 @@ function animationSlide() {
       })
       .setPin(slide, { pushFollowers: false })
       .addTo(controller)
-
-
-
-
-    // nextSlideTl.fromTo(nextSlide, { y: '0%' }, { y: '100%' });
-    // nextSlideTl.fromTo(slide, { opacity: 0, scale: 1 }, { opacity: 1, scale: 0.5 });
-    // nextSlideTl.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
-    // nextSlideScene = new ScrollMagic.Scene({
-    //   triggerElement: slide,
-    //   triggerHook: 0,
-    //   duration: '100%'
-    // })
-    //   .setTween(nextSlideTl)
-    //   .addIndicators({
-    //     colorStart: 'white',
-    //     colorTrigger: 'white',
-    //     name: 'nextSlide',
-    //     indent: 200
-    //   })
-    //   .setPin(slide, { pushFollowers: false })
-    //   .addTo(controller);
-    // ;
   });
 }
 
@@ -85,22 +63,60 @@ const cursorText = cursor.querySelector('span');
 const burger = document.querySelector('.burger');
 
 function cursorFn(e) {
-  // cursor.style.top = cursor.offsetTop + 'px';
   cursor.style.top = e.pageY + 'px';
   cursor.style.left = e.pageX + 'px';
   cursor.setAttribute('data-offset', cursor.offsetTop - window.scrollY)
-  // console.log(cursor.offsetTop)
 }
 function cursorFnScroll(e) {
   const fromTop = cursor.getAttribute('data-offset');
   cursor.style.top = parseInt(fromTop, 10) + scrollY + 'px';
-  console.log('from top=' + fromTop);
-  console.log('scrollY=' + scrollY);
+  // console.log('from top=' + fromTop);
+  // console.log('scrollY=' + scrollY);
 }
 
+function activeCursor(e) {
+  // console.log(e.target)
+  if (e.target.id === 'logo' || e.target.classList.contains('burger')) {
+    cursor.classList.add('nav-active');
+  } else {
+    cursor.classList.remove('nav-active');
+  }
+  if (e.target.classList.contains('explore')) {
+    cursor.classList.add('explore-active');
+    gsap.to('.title-swipe', 1, { y: '0%' });
+    cursorText.innerText = "Tap";
+  } else {
+    cursor.classList.remove('explore-active');
+    gsap.to('.title-swipe', 1, { y: '100%' });
+    cursorText.innerText = '';
+  }
+}
+function toggleNav(e) {
+  console.log(e.target)
+  if (!e.target.classList.contains('active')) {
+    e.target.classList.add('active');
+    gsap.to('.line1', .5, { rotate: '45', y: 5, background: 'black' });
+    gsap.to('.line2', .5, { rotate: '-45', y: -5, background: 'black' });
+    gsap.to('#logo', .5, { color: 'black' });
+    gsap.to('.nav-bar', .5, { clipPath: 'circle(2500px at 100% -10%)' });
+    document.body.classList.add('hide');
+    cursor.style.borderColor = 'black';
+    cursor.style.zIndex = 2;
+  } else {
+    e.target.classList.remove('active');
+    gsap.to('.line1', .5, { rotate: '0', y: 0, background: 'white' });
+    gsap.to('.line2', .5, { rotate: '0', y: 0, background: 'white' });
+    gsap.to('#logo', .5, { color: 'white' });
+    gsap.to('.nav-bar', .5, { clipPath: 'circle(0px at 100% -10%)' });
+    document.body.classList.remove('hide');
+    cursor.style.borderColor = 'white';
+    cursor.style.zIndex = 1;
+  }
+}
 // events
-
+window.addEventListener('mouseover', activeCursor)
 window.addEventListener('mousemove', cursorFn);
 window.addEventListener('scroll', cursorFnScroll);
+burger.addEventListener('click', toggleNav)
 
 animationSlide();
