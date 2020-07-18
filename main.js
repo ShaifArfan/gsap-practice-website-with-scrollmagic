8e007498fc689802f60f4dfb6df5eb5f84897d82
Shaifullah Arfan <shaifullaharfan17@gmail.com>
2020-07-18 18:02:53 +0600

    adding custom cursor

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let controller;
 let slideScene;
-let pageScene;
+let nextSlideScene;
 let detailScene;
 
 function animationSlide() {
@@ -18,7 +18,7 @@ function animationSlide() {
     });
     slideTl.fromTo(revealImg, { x: '0%' }, { x: '100%' });
     slideTl.fromTo(slideImg, { scale: 2 }, { scale: 1 }, '-=1');
-    slideTl.fromTo(revealText, { x: '0%' }, { x: '100%' }, '-=.5')
+    slideTl.fromTo(revealText, { x: '0%' }, { x: '100%' }, '-=.5');
     slideScene = new ScrollMagic.Scene({
       triggerElement: slide,
       triggerHook: 0.25,
@@ -32,6 +32,50 @@ function animationSlide() {
       })
       .addTo(controller);
 
+    // adding the next slide animation
+    const nextSlideTl = gsap.timeline();
+    let nextSlide = (slides.length - 1) === index ? 'end' : slides[index + 1];
+
+    // nextSlideTl.fromTo(nextSlide, { opacity: 0 }, { opacity: 1 });
+    nextSlideTl.fromTo(nextSlide, { y: '0%' }, { y: '50%' })
+    nextSlideTl.fromTo(slide, { scale: 1, opacity: 1 }, { scale: .5, opacity: 0 })
+    nextSlideTl.fromTo(nextSlide, { y: '50%' }, { y: '0%' }, '-=.5')
+
+    nextSlideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0,
+      duration: '100%'
+    }).setTween(nextSlideTl)
+      .addIndicators({
+        colorStart: 'red',
+        colorTrigger: 'red',
+        name: 'nextSl',
+        indent: 150
+      })
+      .setPin(slide, { pushFollowers: false })
+      .addTo(controller)
+
+
+
+
+    // nextSlideTl.fromTo(nextSlide, { y: '0%' }, { y: '100%' });
+    // nextSlideTl.fromTo(slide, { opacity: 0, scale: 1 }, { opacity: 1, scale: 0.5 });
+    // nextSlideTl.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
+    // nextSlideScene = new ScrollMagic.Scene({
+    //   triggerElement: slide,
+    //   triggerHook: 0,
+    //   duration: '100%'
+    // })
+    //   .setTween(nextSlideTl)
+    //   .addIndicators({
+    //     colorStart: 'white',
+    //     colorTrigger: 'white',
+    //     name: 'nextSlide',
+    //     indent: 200
+    //   })
+    //   .setPin(slide, { pushFollowers: false })
+    //   .addTo(controller);
+    // ;
   });
 }
 
@@ -39,5 +83,24 @@ function animationSlide() {
 const cursor = document.querySelector('.cursor');
 const cursorText = cursor.querySelector('span');
 const burger = document.querySelector('.burger');
+
+function cursorFn(e) {
+  // cursor.style.top = cursor.offsetTop + 'px';
+  cursor.style.top = e.pageY + 'px';
+  cursor.style.left = e.pageX + 'px';
+  cursor.setAttribute('data-offset', cursor.offsetTop - window.scrollY)
+  // console.log(cursor.offsetTop)
+}
+function cursorFnScroll(e) {
+  const fromTop = cursor.getAttribute('data-offset');
+  cursor.style.top = parseInt(fromTop, 10) + scrollY + 'px';
+  console.log('from top=' + fromTop);
+  console.log('scrollY=' + scrollY);
+}
+
+// events
+
+window.addEventListener('mousemove', cursorFn);
+window.addEventListener('scroll', cursorFnScroll);
 
 animationSlide();
